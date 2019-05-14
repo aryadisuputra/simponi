d9897762b0a53eb2008678ae998a66891d0f1894
Bayu Wira <bayuwira1718@gmail.com>
2019-05-14 11:12:37 +0800

    tambah dikit

--- app/router.php
+++ app/router.php
@@ -30,15 +30,15 @@ $routes['control-panel/edukasi'] = 'EdukasiController@index';
 
 // pengguna
 $routes['control-panel/pengguna'] = 'PenggunaController@index';
-<<<<<<< HEAD
+
 $routes['control-panel/kategori-edukasi'] = 'KategoriEdukasiController@index';
 $routes['control-panel/kategori-produk'] = 'KategoriProdukController@index';
 $routes['control-panel/produk'] = 'ProdukController@index';
-=======
+
 $routes['control-panel/pengguna/add'] = 'PenggunaController@add';
 $routes['control-panel/pengguna/create'] = 'PenggunaController@create';
 
->>>>>>> 3ce49f5613abd37df3b2cafcf86da08e4bd4b87b
+
 // autentikasi
 $routes['login'] = 'LandingController@login';
 $routes['dologin'] = 'LandingController@doLogin';
